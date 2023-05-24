ecf30176097bc6193985438be8d8ed4ca5220a6b
laiducav <khoa20082001@gmail.com>
2023-05-24 09:41:53 +0700

    bug fix

--- components/bus-details/tabs/Tabs.jsx
+++ components/bus-details/tabs/Tabs.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { TouchableOpacity, FlatList, Text, View } from "react-native";
 
-import styles from "./tabs.style";
+import styles from "./Tabs.style";
 import { SIZES } from "../../../constants";
 
 function TabButton({ name, activeTab, onHandleSearchType }) {
