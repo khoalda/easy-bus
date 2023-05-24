85c1b7fe2708119de6cbbe22b097462cb0e5b8f5
Lai Duc Anh Khoa <khoa20082001@gmail.com>
2023-05-24 23:36:00 +0700

    update

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ import { Redirect, SplashScreen } from "expo-router";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useEffect, useState } from "react";
 export default function Index() {
-  const [showOnboarding, setShowOnboarding] = useState(false);
+  const [showOnboarding, setShowOnboarding] = useState(true);
   useEffect(() => {
     AsyncStorage.getItem("onboardingCompleted").then((value) => {
       if (value) {
