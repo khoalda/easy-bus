4a4956170ff935d667073271bd08bdb54253cfd1
Lai Duc Anh Khoa <khoa20082001@gmail.com>
2023-05-27 23:30:25 +0700

    update

--- app/index.js
+++ app/index.js
@@ -1,6 +1,14 @@
 import { Redirect, SplashScreen } from "expo-router";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useEffect, useState } from "react";
+import * as Sentry from 'sentry-expo';
+
+Sentry.init({
+  dsn: 'YOUR DSN HERE',
+  enableInExpoDevelopment: true,
+  debug: true, // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
+});
+
 export default function Index() {
   const [showOnboarding, setShowOnboarding] = useState(true);
   useEffect(() => {
